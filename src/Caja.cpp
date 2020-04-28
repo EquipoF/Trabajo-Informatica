f07b9981e1324da1c2ad9cd55624cae7f7637ad2
Sergio Martínez López-Villalta <SMLV_97@hotmail.com>
2020-04-28 20:57:35 +0200

    Creacion de las plataformas mundo 1

--- src/Caja.cpp
+++ src/Caja.cpp
@@ -4,9 +4,9 @@
 Caja::Caja()
 {
 	suelo = Rectangulo(20.0, 2.0, Vector2D(0, -7)); // La caja tiene, con las dimensiones de ventana 800*600, un ancho de 20 y un alto de 14
-	techo = Rectangulo(20.0, 1.0, Vector2D(0, 7));
-	paredD = Rectangulo(2.0, 14.0, Vector2D(10, 0));
-	paredI = Rectangulo(2.0, 14.0, Vector2D(-10, 0));
+	techo = Rectangulo(20.0, 1.0, Vector2D(0, 43));
+	paredD = Rectangulo(2.0, 50.0, Vector2D(10, 18));
+	paredI = Rectangulo(2.0, 50.0, Vector2D(-10, 18));
 }
 
 Caja::~Caja()
